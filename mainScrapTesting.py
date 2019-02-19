ecbce005f29b4cde074757c933d9ab123bd6773c
BetaSpud <47744711+BetaSpud@users.noreply.github.com>
2019-02-19 16:12:29 +1000

    Add files via upload

---
+++ mainScrapTesting.py
@@ -0,0 +1,9 @@
+nameList = ["John"] # array.. list in python;
+
+def main():
+    print("Hello world")
+    nameList.append("Jackson")
+    for x in range(len(nameList)): #makes a list of int for the loop to assign to x (in this case [0,1]); 
+     print(nameList[x])
+main()
+
