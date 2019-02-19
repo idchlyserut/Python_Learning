928cb147a840379c184cc8af925fa3081e8a8a5f
BetaSpud <47744711+BetaSpud@users.noreply.github.com>
2019-02-19 21:21:06 +1000

    Add files via upload

---
+++ Exercise1-1(strend).py
@@ -0,0 +1,5 @@
+t = ['T','a','d','o',' ','i','s',' ','t','h','e',' ','c','a','n','d','y',' ','b','e','e','t','l','e']
+#d = ['S','p','u','d',' ','l','i','k','e','s',' ','t','o',' ','e','a','t',' ','b','e','e','t','l','e']
+a = ['b','e','e','t','l','e']
+
+#Write a function strend(t,a), which returns 1 if the list a occurs at the end of the list t, and zero otherwise
