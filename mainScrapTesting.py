66aed7fae9bff54739278e3384807ca2fbcbe1f7
BetaSpud <47744711+BetaSpud@users.noreply.github.com>
2019-02-20 10:31:45 +1000

    Update mainScrapTesting.py

--- mainScrapTesting.py
+++ mainScrapTesting.py
@@ -1,4 +1,13 @@
+
+class custList:
+     def __init__(self,x):
+         self.val = x
+     def eir(self):    
+      return range(len(self.val))
+               
+
 nameList = ["John"] # array.. list in python;
+testNameList = custList(["david","greg","will"])
 
 def eir(a): #makes a list of int for the loop to assign
      if isinstance(a, list):
@@ -9,6 +18,9 @@ def main():
     nameList.append("Jackson")    
     for x in eir(nameList): 
      print(nameList[x])
-
+    for y in testNameList.eir():
+         print(testNameList.val[y])
+    testNameList.val.extend(["idiey","popschin"])
+    for y in testNameList.eir():
+         print(testNameList.val[y])      
 main()
-
